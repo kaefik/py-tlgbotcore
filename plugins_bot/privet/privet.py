23a4b0344b755bdc0ed8d42be667d3297262e3c7
Ilnur Saifutdinov <ilnursoft@gmail.com>
2021-07-11 14:13:12 +0300

    fix

--- plugins_bot/privet/privet.py
+++ plugins_bot/privet/privet.py
@@ -7,4 +7,6 @@ from telethon import events
 
 @tlgbot.on(events.NewMessage(pattern='hi'))
 async def handler(event):
-    await event.reply('hey')
+    with open('README.md') as f:
+        s = f.read()
+    await event.reply(f'{s}')
