60f1e91c3b6875422cd02551e222e5c8a25eadfa
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2021-07-13 00:06:47 +0300

    add фильтрации пользователей которые имеют доступ к плагинам бота

--- plugins_bot/privet/privet.py
+++ plugins_bot/privet/privet.py
@@ -5,8 +5,8 @@ Example plugins for tlgbotcore (send hi)
 from telethon import events
 
 
-@tlgbot.on(events.NewMessage(pattern='hi'))
+@tlgbot.on(events.NewMessage(chats=tlgbot.settings.get_all_user_id(), pattern='hi'))
 async def handler(event):
     # with open('README.md') as f:
     #     s = f.read()
-    await event.reply('ЩЛ')
+    await event.reply('Привет!')
