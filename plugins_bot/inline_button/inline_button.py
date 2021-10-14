279bce0aa6999658fa8e1cb6931f203717d502b8
ILnur Saifutdinov <ilnursoft@gmail.com>
2021-10-14 17:34:02 +0300

    add button for links

--- plugins_bot/inline_button/inline_button.py
+++ plugins_bot/inline_button/inline_button.py
@@ -8,7 +8,7 @@ from telethon import events, Button
 button_main_cmd = [
     [Button.inline("сегодня"),
      Button.inline("завтра")],
-    [Button.inline("табель"),
+    [Button.url('Check this site!', 'https://lonamiwebs.github.io'),
      Button.inline("любой день")]
 ]
 
@@ -16,6 +16,8 @@ button_main_cmd = [
 @tlgbot.on(events.NewMessage(chats=tlgbot.settings.get_all_user_id(), pattern='/inline'))
 async def start_cmd_plugin(event):
     await event.respond("Выбери ", buttons=button_main_cmd)
+    # answer = await event.wait_event(events.CallbackQuery())
+    # print(answer.data.decode("utf-8"))
 
 
 @tlgbot.on(events.CallbackQuery(pattern='сегодня|завтра|табель|любой день'))
