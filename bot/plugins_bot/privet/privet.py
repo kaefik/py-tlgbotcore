3f42dd548fe52a4b95e6e59526afce8e9af5075f
ilnursoft <ilnursoft@gmail.com>
2025-09-10 10:35:26 +0300

    fix команда   hi  только для администраторов

--- bot/plugins_bot/privet/privet.py
+++ bot/plugins_bot/privet/privet.py
@@ -5,7 +5,7 @@ Example plugins for tlgbotcore (send hi)
 from telethon import events
 
 
-@tlgbot.on(tlgbot.cmd('hi'))
+@tlgbot.on(tlgbot.admin_cmd('hi'))
 async def handler(event):
     # with open('README.md') as f:
     #     s = f.read()
